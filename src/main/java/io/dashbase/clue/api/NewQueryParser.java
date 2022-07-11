004c166f677488d99e88d2e9e96491d9c0a559f6
Bibek Panthi <bpanthi@Bibeks-MacBook-Pro.local>
2022-07-11 10:28:00 +0545

    allow searching with wild(*) prefix

---
+++ src/main/java/io/dashbase/clue/api/NewQueryParser.java
@@ -0,0 +1,14 @@
+package io.dashbase.clue.api;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.queryparser.classic.QueryParser;
+
+public class NewQueryParser extends QueryParser {
+
+        public NewQueryParser(String defaultField, Analyzer analyzer) {
+	    super(defaultField, analyzer);
+	    setAutoGeneratePhraseQueries(true);
+	    setDefaultOperator(QueryParser.Operator.AND);
+	    setAllowLeadingWildcard(true);
+	}
+}
